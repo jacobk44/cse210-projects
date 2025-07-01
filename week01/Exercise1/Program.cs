6d73b6eaaaf3acb8a6cfac2e5e58b023bf894901
jacobkwartengamoah@gmail.com <>
2025-07-01 19:14:33 +0000

    I added a new code

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,7 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
     }
 }
 
