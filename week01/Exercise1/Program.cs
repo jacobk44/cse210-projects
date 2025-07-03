72f585828d6f6b305d9b9c8d5369615e3d50ea28
jacobkwartengamoah@gmail.com <>
2025-07-03 00:58:14 +0000

    code added at exercise1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,8 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your favorite color? ");
-        string color = Console.ReadLine();
+        Console.Write("What is your first name?");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
 
