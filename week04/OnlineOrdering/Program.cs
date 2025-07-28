2c4c2288b1bb3ee5ba3bfdb827c5ce8096cb4e28
jacobkwartengamoah@gmail.com <>
2025-07-28 07:00:23 +0000

    I added one order to it

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -20,6 +20,27 @@ class Program
         Console.WriteLine($"\nTotalPrice: {order1.GetTotalCost():0.00}");
 
 
+        Console.WriteLine("................................................");
+        Console.WriteLine();
+
+
+        // Create Address and Customer
+        Address address2 = new Address("Odikoman", "Nungua", "GreaterAccra", "GHANA");
+        Customer customer2 = new Customer("James Matt", address2);
+
+        // Create Order and Add Product
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("iphones", "009", 78.99m, 4));
+        order2.AddProduct(new Product("TLC tv", "102", 700.99m, 10));
+
+        // Display result
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine();
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"\nTotalPrice: {order2.GetTotalCost():0.00}");
+
+        
+
 
     }
 }
