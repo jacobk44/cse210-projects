4bad8df04b7267e75a68e5ec798c7ea597c77d4b
jacobkwartengamoah@gmail.com <>
2025-08-09 17:02:34 +0000

    Shapes code done

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Red", 5);
+        shapes.Add(square);
+        Console.WriteLine();
+
+
+        Circle circle = new Circle("Blue", 9);
+        shapes.Add(circle);
+        Console.WriteLine();
+
+
+        Rectangle rectangle = new Rectangle("Pink", 9, 5);
+        shapes.Add(rectangle);
+        Console.WriteLine();
+
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+
+            Console.WriteLine($"The {color} shape has an area of {area:F2}");
+        }
+
+
     }
 }
